6509aad4f377d6521c8e45c33e2ab889c2d783c3
macyrate <mcr810@live.com>
2020-01-12 15:18:50 +0800

    制作了启动界面

---
+++ Touhou Hakuroukan/Assets/Scripts/StartMenuLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuLoader : MonoBehaviour
+{
+    private AsyncOperation operation;
+
+    IEnumerator AsyncLoading()
+    {
+        yield return new WaitForSeconds(3);     //加载太快了，先播上3s的动画
+        operation = SceneManager.LoadSceneAsync("StartMenuScene", mode: LoadSceneMode.Single);
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+            yield return null;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(AsyncLoading());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
