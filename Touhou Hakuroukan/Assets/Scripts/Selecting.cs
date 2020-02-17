5089fe4752e288953a326d32cf58c242a32ce509
macyrate <mcr810@live.com>
2020-02-17 23:18:40 +0800

    添加TextMesh Pro

---
+++ Touhou Hakuroukan/Assets/Scripts/Selecting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecting : MonoBehaviour
+{
+    Animator animator;
+
+    public void SelectingEffect()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("selected");
+    }
+
+}
