b1f667f5a5a1a24c2cd96056b4521f6a98064c6f
macyrate <mcr810@live.com>
2020-02-20 23:23:13 +0800

    管理主菜单音效

---
+++ Touhou Hakuroukan/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip seCancel;
+    public AudioClip seSelect;
+    public AudioClip seOK;
+    public AudioClip seInvalid;
+
+    public AudioSource seAudioSource;
+
+    public void PlaySingle(AudioClip clip)
+    {
+        seAudioSource.clip = clip;
+        seAudioSource.Play();
+    }
+}
