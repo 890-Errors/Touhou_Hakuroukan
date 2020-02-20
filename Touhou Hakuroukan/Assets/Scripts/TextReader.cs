4a429a3bfba2829bd125a4056fbf3459b23bc61b
macyrate <mcr810@live.com>
2020-02-20 23:24:29 +0800

    补充菜单内文档

---
+++ Touhou Hakuroukan/Assets/Scripts/TextReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextReader : MonoBehaviour
+{
+    public string pathToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset txt = Resources.Load(pathToLoad) as TextAsset;
+        gameObject.GetComponent<Text>().text = txt.text;
+    }
+}
