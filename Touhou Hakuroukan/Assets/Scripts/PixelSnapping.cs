5f2bc4a3ed7ed36e5170798a6be45a9381281237
macyrate <mcr810@live.com>
2020-02-25 21:58:16 +0800

    主菜单加入PixelSnapping选项

---
+++ Touhou Hakuroukan/Assets/Scripts/PixelSnapping.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PixelSnapping : MonoBehaviour
+{
+    public TextMeshProUGUI textPixelSnapping;
+    private bool isPixelSnapping = false;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        textPixelSnapping = gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        textPixelSnapping.text = "PixelSnapping  " + (isPixelSnapping ? "ON" : "OFF");
+
+    }
+
+    public void ChangePixelSnapping()
+    {
+        isPixelSnapping = !isPixelSnapping;
+        textPixelSnapping.text = "PixelSnapping  " + (isPixelSnapping ? "ON" : "OFF");
+    }
+}
