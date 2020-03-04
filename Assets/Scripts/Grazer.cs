39b16caf6f6b64237c5a47b0ca04da537dd1a3ed
macyrate <mcr810@live.com>
2020-03-04 18:48:13 +0800

    实现自机擦弹功能的原型

---
+++ Assets/Scripts/Grazer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DanmakU;
+
+public class Grazer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip seGraze;
+    public int grazeNum;
+
+    private DanmakuCollisionList danmakuCollisionsLastFrame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        GetComponent<DanmakuCollider>().OnDanmakuCollision += Graze;
+    }
+
+    //擦弹
+    void Graze(DanmakuCollisionList danmakuCollisions)
+    {
+        //danmakuCollisionsLastFrame??
+        
+        audioSource.PlayOneShot(seGraze);   //擦弹音效
+
+        grazeNum += danmakuCollisions.Count;
+        //TODO:擦弹回蓝
+        
+    }
+}
