581c13384906fbd9fde00d5f26be05f4881e4c9f
macyrate <mcr810@live.com>
2020-03-04 18:43:13 +0800

    重载AudioManager的PlaySingle()方法

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip seCancel;
+    public AudioClip seSelect;
+    public AudioClip seOK;
+    public AudioClip seInvalid;
+
+    public AudioSource seAudioSource;
+
+    public void PlaySingle(AudioClip clip)
+    {
+        seAudioSource.clip = clip;
+        seAudioSource.Play();
+    }
+
+    public void PlaySingle(string clip)
+    {
+        switch (clip)
+        {
+            case "Cancel":
+                PlaySingle(seCancel);
+                break;
+            case "Select":
+                PlaySingle(seSelect);
+                break;
+            case "OK":
+                PlaySingle(seOK);
+                break;
+            case "Invalid":
+                PlaySingle(seInvalid);
+                break;
+            default:
+                throw new System.Exception("Invalid name of sound effect.");
+        }
+    }
+
+
+}
