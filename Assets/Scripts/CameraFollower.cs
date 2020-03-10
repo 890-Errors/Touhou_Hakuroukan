16451e0a4d698c5b590e997c5a17593d61a34eeb
macyrate <mcr810@live.com>
2020-03-10 22:47:57 +0800

    基础的摄像机跟随

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public GameObject FollowingObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(
+            FollowingObject.transform.position.x, 
+            FollowingObject.transform.position.y, 
+            gameObject.transform.position.z
+            );
+    }
+}
