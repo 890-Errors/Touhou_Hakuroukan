84123a0b9a9a06ac9102be61cee0d75cb2c9bddc
macyrate <mcr810@live.com>
2020-03-10 21:05:30 +0800

    修复“我杀我自己”的问题

---
+++ Assets/Scripts/WhoseDanmaku.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DanmakU;
+
+public class WhoseDanmaku : MonoBehaviour
+{
+    public static bool IsMyDanmaku(Danmaku danmaku, DanmakuEmitter myEmitter) 
+        => myEmitter.Speed.GetValue() == danmaku.Speed;
+}
