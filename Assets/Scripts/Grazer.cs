c49a3649b407bac2119a5b667e11d56815863669
macyrate <mcr810@live.com>
2020-03-10 21:06:18 +0800

    修复自机子弹引发擦弹的问题

--- Assets/Scripts/Grazer.cs
+++ Assets/Scripts/Grazer.cs
@@ -8,25 +8,36 @@ public class Grazer : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip seGraze;
     public int grazeNum;
+    public DanmakuEmitter emitter;
 
     private DanmakuCollisionList danmakuCollisionsLastFrame;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         audioSource = GetComponent<AudioSource>();
-        GetComponent<DanmakuCollider>().OnDanmakuCollision += Graze;
+        emitter = transform.parent.GetComponent<Player>().emitter;
+
+        GetComponent<DanmakuCollider>().OnDanmakuCollision += Graze;    //注册碰撞事件处理（擦弹）
     }
 
     //擦弹
     void Graze(DanmakuCollisionList danmakuCollisions)
     {
-        //danmakuCollisionsLastFrame??
-        
-        audioSource.PlayOneShot(seGraze);   //擦弹音效
+        for (int i = 0; i < danmakuCollisions.Count; i++)
+        {
+            //TODO: 较好的检测自己子弹办法？
+            if (!WhoseDanmaku.IsMyDanmaku(danmakuCollisions[i].Danmaku, emitter) 
+                && !danmakuCollisions[i].Danmaku.Grazed)            //用弹速检测是否是自己的子弹
+            {
+                var thisDanmaku = danmakuCollisions[i].Danmaku;     //danmakuCollisons是readonly的
+                thisDanmaku.Grazed = true;
 
-        grazeNum += danmakuCollisions.Count;
-        //TODO:擦弹回蓝
-        
+                audioSource.PlayOneShot(seGraze);   //擦弹音效
+
+                grazeNum += danmakuCollisions.Count;
+                //TODO: 擦弹回蓝
+            }
+        }
     }
 }
