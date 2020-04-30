ffbea8ca94ee1102ca469d34143090bb3b810ee2
macyrate <mcr810@live.com>
2020-04-30 16:19:56 +0800

    线性插值实现更平滑的摄像机跟随

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -1,16 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Security.Cryptography;
+using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
     public GameObject FollowingObject;
+    public float easing = 0.05f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        gameObject.transform.position = new Vector3(
+        Vector3 destination = new Vector3(
             FollowingObject.transform.position.x,
             FollowingObject.transform.position.y,
             gameObject.transform.position.z
             );
+        Vector3 smoothDestination = Vector3.Lerp(gameObject.transform.position, destination, easing);
+        gameObject.transform.position = smoothDestination;
+
     }
 }
