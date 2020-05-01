43facc17462e9b75e7a1c9c6ef4354ecb4874ad6
macyrate <mcr810@live.com>
2020-05-01 22:46:47 +0800

    优化摄像机关注点

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -3,18 +3,38 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    public GameObject FollowingObject;
-    public float easing = 0.05f;
+    public GameObject followingObject;
+    public Player player;
+    public float smoothRate = 0.05f;
+    public float betweenRate = 0.3f;
+    Vector3 destination;
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 destination = new Vector3(
-            FollowingObject.transform.position.x,
-            FollowingObject.transform.position.y,
+        //获取跟踪目标的位置
+        if (player.gameObject == followingObject && player.enemy)
+        {
+            destination = Vector3.Lerp(
+                player.gameObject.transform.position,
+                player.enemy.transform.position,
+                betweenRate);
+            destination.z = gameObject.transform.position.z;
+        }
+        else
+        {
+            destination = new Vector3(
+            followingObject.transform.position.x,
+            followingObject.transform.position.y,
             gameObject.transform.position.z
             );
-        Vector3 smoothDestination = Vector3.Lerp(gameObject.transform.position, destination, easing);
+        }
+
+        //线性插值平滑摄像机移动
+        Vector3 smoothDestination = Vector3.Lerp(
+            gameObject.transform.position, 
+            destination, 
+            smoothRate);
+        //更新摄像机位置
         gameObject.transform.position = smoothDestination;
 
     }
