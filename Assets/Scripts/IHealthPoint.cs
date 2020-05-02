09de7a7b0a4f3a0c63ea76636d2d63bd0e7947d6
macyrate <mcr810@live.com>
2020-05-02 19:31:18 +0800

    定义两个接口

---
+++ Assets/Scripts/IHealthPoint.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DanmakU;
+
+public interface IHealthPoint
+{
+    int HP { get; set; }
+}
--- Assets/Scripts/IHitbox.cs
+++ Assets/Scripts/IHitbox.cs
@@ -1,4 +1,12 @@
-﻿public interface IHitbox
+﻿using UnityEngine;
+using DanmakU;
+
+public interface IHitbox
 {
+    Collider2D Collider2D { get; set; }
+    DanmakuCollider DanmakuCollider { get; set; }
+    IHealthPoint ParentController { get; set; }
+
+    void OnDanmakuCollision(DanmakuCollisionList danmakuCollisions);
 
 }
