f486a9f3576c30b901cd68a49cd08ef664e2b5ab
macyrate <mcr810@live.com>
2020-05-07 00:03:48 +0800

    实现自机符卡系统

---
+++ Assets/Scripts/ISpellCard.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using DanmakU;
+
+public interface ISpellCard
+{
+    int Cost { get; set; }
+    int ID { get; set; }
+    string Name { get; set; }
+    string Desc { get; set; }
+    void SpellCardRelease();
+}
+
+public interface ISpellCardPlayer : ISpellCard
+{
+    Player Player { get; set; }
+}
+
+public interface ISpellCardEnemy : ISpellCard
+{
+    Player Player { get; set; }
+}
---
+++ Assets/Scripts/SpellCard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface SpellCard
+{
+    int ID { get; }
+    int Cost { get; }
+    string SpellCardName { get; }
+    string SpellCardDesc { get; }
+
+    void SpellCardRelease();
+}
