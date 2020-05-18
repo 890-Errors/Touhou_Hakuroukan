9dd8fd73edc24d490d251bb6903a75779dc62d51
macyrate <mcr810@live.com>
2020-05-18 23:10:46 +0800

    为还没实现效果的两张符卡占个坑

---
+++ Assets/Scripts/SpellCards/Kekkafuzan.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class Kekkafuzan : MonoBehaviour, ISpellCard
+{
+    public int ID { get; }
+    public int Cost { get; }
+    public string SpellCardName { get; }
+    public string SpellCardDesc { get; }
+
+    public Player Player;
+    public LifeUIController LifeUIController;
+
+    public Kekkafuzan()
+    {
+        ID = 1;
+        Cost = 1;
+        SpellCardName = "结跏趺斩";
+        SpellCardDesc =
+            "同时斩下楼观剑和白楼剑，\n" +
+            "放出绿色的剑风阻挡弹幕。";
+    }
+
+    //TODO: 可以用特性重写
+    public void SpellCardRelease()
+    {
+        Grazer grazer = Player.grazer;
+        if (grazer.grazeLevel < Cost)
+        {
+            CameraShaker.Instance.ShakeOnce(10f, 4f, .2f, .2f);     //擦弹等级不足
+            AudioManager.instance.PlaySingle("Invalid");
+        }
+        else
+        {
+            grazer.grazeLevel -= Cost;
+            grazer.GrazeLevelUIController.SetGrazeLevel(grazer.grazeLevel);
+            LifeUIController.SetLifeLevel(++Player.HP);
+        }
+    }
+}
---
+++ Assets/Scripts/SpellCards/Miraieigouzan.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class Miraieigouzan : MonoBehaviour, ISpellCard
+{
+    public int ID { get; }
+    public int Cost { get; }
+    public string SpellCardName { get; }
+    public string SpellCardDesc { get; }
+
+    public Player Player;
+    public LifeUIController LifeUIController;
+
+    public Miraieigouzan()
+    {
+        ID = 5;
+        Cost = 5;
+        SpellCardName = "人鬼「未来永劫斩」";
+        SpellCardDesc =
+            "向对手冲刺斩击后，再进行连续追击。\n" +
+            "威力极高且拥有无敌时间，可谓是攻防一体的完美符卡。";
+    }
+
+    //TODO: 可以用特性重写
+    public void SpellCardRelease()
+    {
+        Grazer grazer = Player.grazer;
+        if (grazer.grazeLevel < Cost)
+        {
+            CameraShaker.Instance.ShakeOnce(10f, 4f, .2f, .2f);     //擦弹等级不足
+            AudioManager.instance.PlaySingle("Invalid");
+        }
+        else
+        {
+            grazer.grazeLevel -= Cost;
+            grazer.GrazeLevelUIController.SetGrazeLevel(grazer.grazeLevel);
+            LifeUIController.SetLifeLevel(++Player.HP);
+        }
+    }
+}
